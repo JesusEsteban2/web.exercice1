57a04dbf0ba98294e5cc573f2ef1acb0d799d727
Mananas <Mananas>
2024-12-20 12:49:51 +0100

    Add json server

--- asynchrony.js
+++ asynchrony.js
@@ -11,10 +11,10 @@ const makeAsync = (time) => {
   });
 };
 
-makeAsync(1000)
-  .then(console.log(data) /*Si se cumple la promesa*/)
-  .catch(console.error(error.message) /*Si no se cumple la promesa*/)
-  .finally(console.log('Proceso finalizado') /*Se ejecuta en ambos casos*/);
+// makeAsync(1000)
+//   .then(console.log(data) /*Si se cumple la promesa*/)
+//   .catch(console.error(error.message) /*Si no se cumple la promesa*/)
+//   .finally(console.log('Proceso finalizado') /*Se ejecuta en ambos casos*/);
 
 //Funciones async
 async function foo() {
@@ -32,3 +32,32 @@ x.then((n) => console.log(n));
 async function baz() {
   const p = await foo();
 }
+
+// Fetch
+
+function jsonFech() {
+  const url = 'https://dummyjson.com/userss';
+
+  const r = fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        //throw new Error('Error fecht ha fallado');
+        return response.status + ' ' + response.statusText;
+      }
+    })
+    .then((data) => {
+      if (typeof data === 'string') {
+        console.log('Error: ', data);
+        return;
+      }
+      console.log(data.total);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      console.log('Continua después del error');
+    });
+}
+
+jsonFech();
